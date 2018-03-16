6275203ef1b597b11eb9aa75d5fe46aa776f291c
Dana <dankasavchuk@gmail.com>
2018-03-16 11:48:33 +0100

    menu with recursion

--- index.js
+++ index.js
@@ -9,7 +9,31 @@ const data = {
                 {
                     "title": "item 1.1",
                     "handler": "",
-                    "disabled": false
+                    "disabled": false,
+                    "subitems": [
+                        {
+                            "title": "item 1.1.1",
+                            "handler": "",
+                            "disabled": false
+                        },
+                        {
+                            "title": "item 1.1.2",
+                            "handler": "",
+                            "disabled": false,
+                            "subitems": [
+                                {
+                                    "title": "item 1.1.1.1",
+                                    "handler": "",
+                                    "disabled": false
+                                },
+                                {
+                                    "title": "item 1.1.1.2",
+                                    "handler": "",
+                                    "disabled": false
+                                }
+                            ]
+                        }
+                    ]
                 },
                 {
                     "title": "item 1.2",
@@ -31,7 +55,19 @@ const data = {
                 {
                     "title": "item 2.2",
                     "handler": "",
-                    "disabled": false
+                    "disabled": false,
+                    "subitems": [
+                        {
+                            "title": "item 2.2.1",
+                            "handler": "",
+                            "disabled": false
+                        },
+                        {
+                            "title": "item 2.2.2",
+                            "handler": "",
+                            "disabled": false
+                        }
+                    ]
                 }
             ]
         },
@@ -55,68 +91,57 @@ const data = {
         {
             "title": "item 4",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 5",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 6",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 7",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 8",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 9",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 10",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 11",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 12",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 13",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         },
         {
             "title": "item 14",
             "handler": "",
-            "disabled": false,
-            "subitems": []
+            "disabled": false
         }
     ]
 };
@@ -127,9 +152,7 @@ var SETTINGS = {
     navBarMoveDistance: 50
 };
 
-var container = document.getElementById(data.elementId);
-// Menu(container, data.menu);
-
+createMenu();
 
 function showMenu() {
     document.getElementById("dropdownMenu").classList.toggle("show");
@@ -289,27 +312,30 @@ window.onclick = function (event) {
     }
 };
 
-function Menu(container, items) {
-
+function createMenu() {
     var nav = document.createElement('nav');
     nav.className = "nav-menu";
     nav.id = "navigation-container";
     var ul = document.createElement('ul');
     ul.id = "menu";
     ul.className = "menu";
+    document.getElementById(data.elementId).appendChild(nav).appendChild(ul);
+    goArrays(ul, data.menu);
 
-    container.appendChild(nav).appendChild(ul);
+}
 
+function goArrays(container, items) {
     for (var i = 0; i < items.length; i++) {
-        if (items[i].disabled !== true) {
+        if(items[i].disabled !== true) {
 
             var li = document.createElement('li');
             var a = document.createElement('a');
             a.setAttribute('href', '#');
-            ul.appendChild(li).appendChild(a);
+            container.appendChild(li).appendChild(a);
             a.innerText = items[i].title;
 
-            if (items[i].subitems.length !== 0) {
+            if (items[i].hasOwnProperty('subitems')) {
+
                 var iconFontAwesome = document.createElement('i');
                 iconFontAwesome.className = "fas fa-angle-right";
                 a.appendChild(iconFontAwesome);
@@ -319,20 +345,11 @@ function Menu(container, items) {
                 var ulSubItems = document.createElement('ul');
                 ulSubItems.className = "sub-menu";
 
-                li.appendChild(wrapperSubMenu).appendChild(ulSubItems);
+                li.appendChild(wrapperSubMenu)
+                    .appendChild(ulSubItems);
 
-                for (var j = 0; j < items[i].subitems.length; j++) {
-                    if (items[i].subitems[j].disabled !== true) {
-                        var liSubItem = document.createElement('li');
-                        var aSub = document.createElement('a');
-                        aSub.setAttribute('href', '#');
-                        ulSubItems.appendChild(liSubItem).appendChild(aSub);
-                        aSub.innerText = items[i].subitems[j].title;
-                    }
-                }
-
+                goArrays(ulSubItems, items[i].subitems);
             }
         }
     }
-
 }
